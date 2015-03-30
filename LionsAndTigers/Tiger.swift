5d6ae2209969f0889b58e09d52fab055524586d8
jonasbaer <jonas.baer@gmail.com>
2015-03-30 19:47:29 +0700

    first commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,23 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Jonas Baer on 30.03.15.
+//  Copyright (c) 2015 jonas-baer.com. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//JB : Here we setup our Struct's
+
+struct Tiger {
+
+    //JB : Adding some Properties
+
+    var age = 0 // var age:Int! - + default value helps :)
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"") // required import of UIKit
+
+}
---
+++ LionsAndTigers/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  LionsAndTigers
+//
+//  Created by Jonas Baer on 30.03.15.
+//  Copyright (c) 2015 jonas-baer.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func nextButtonBarItemPressed(sender: UIBarButtonItem) {
+
+
+    }
+
+
+
+}
+
