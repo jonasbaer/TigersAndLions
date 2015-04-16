a948cad3ecd98d32bb78434222e50e152d1d74ee
jonasbaer <jonas.baer@gmail.com>
2015-04-16 13:12:52 +0800

    another commit

--- LionsAndTigers/Tiger.swift
+++ LionsAndTigers/Tiger.swift
@@ -19,4 +19,51 @@ struct Tiger {
     var name = ""
     var breed = ""
     var image = UIImage(named:"") // required import of UIKit
+
+    func chuff() {
+            println("Tiger: Chuff Chuff")
+    }
+
+
+    func chuffANumberOfTimes (numberOfTimes: Int) {
+        for var chuff = 0; chuff < numberOfTimes; chuff++ {
+            self.chuff() //JB : Have to call function within same class with self. otherwise naming conflict
+        }
+    }
+
+
+    func chuffANumberOfTimes (numberOfTimes: Int, isLoud: Bool) {
+        for var chuffTimes = 1; chuffTimes <= numberOfTimes; chuffTimes++ {
+            if isLoud {
+                self.chuff() //JB : Have to call function within same class with self. otherwise naming conflict
+            }
+            else {
+                println("Tiger: purr purr")
+            }
+        }
+    }
+
+    //JB : Function to update age of tigers
+    func ageInTigerYearsFromAge (regularAge: Int) -> Int {
+        let newAge = regularAge * 3
+        return newAge
+    }
+
+    //JB : Function to show a random fact
+    func randomFact() -> String {
+        let randomNumber = Int(arc4random_uniform(UInt32(3)))
+
+        var randomFact:String
+
+        if randomNumber == 0 {
+            randomFact = "The Tiger is the biggest species in the cat family"
+        }
+        else if randomNumber == 1 {
+            randomFact = "Tigers can reach a length of 3.3 meters"
+        }
+        else {
+            randomFact = "A group of tigers is known as an 'ambush' or 'streak'"
+        }
+        return randomFact
+    }
 }
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -8,58 +8,87 @@
 
 import UIKit
 
-// Type of Class Tiger needed ! ! ! and declared here to ensure global accessibility
-var myTigers:[Tiger] = []
-
-// Created to make sure next() doesn't show up
-var currentIndex = 0
-
 class ViewController: UIViewController {
+
+    // Type of Class Tiger needed ! ! ! and declared here to ensure global accessibility
+    var myTigers:[Tiger] = []
+
+    // Created to make sure next() doesn't show up
+    var currentIndex = 0
 
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var breedLabel: UILabel!
+    @IBOutlet weak var randomFactLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        //JB : Variable which is of type Tiger - only locally available within the function
+        //JB : !!! Variable which is of type Tiger - only locally available within the function
         var myTiger = Tiger() // Key to get access of instances of other class :)
         myTiger.name = "Tigger"
         myTiger.breed = "Bengal"
         myTiger.age = 3
         myTiger.image = UIImage(named:"BengalTiger.jpg")
 
-        myTigers.append(myTiger)
+        //JB : Call function to update age and return value
+        myTiger.age = myTiger.ageInTigerYearsFromAge(myTiger.age)
 
-        println("My tiger's name is \(myTiger.name), its age is \(myTiger.age), its breed is \(myTiger.breed) and its image is \(myTiger.image)")
+        //JB : Fill in array with Elements above
+        self.myTigers.append(myTiger)
 
-        myImageView.image = myTiger.image
-        nameLabel.text = myTiger.name
-        ageLabel.text = "\(myTiger.age)"
-        breedLabel.text = myTiger.breed
+        //JB : Call of function which is declared in Tiger.swift file
+        myTiger.chuff()
+        myTiger.chuffANumberOfTimes(5, isLoud: false)
 
+//        println("My tiger's name is \(myTiger.name), its age is \(myTiger.age), its breed is \(myTiger.breed) and its image is \(myTiger.image)")
+
+        //JB : Declaration for the first launch of the App
+        self.myImageView.image = myTiger.image
+        self.nameLabel.text = myTiger.name
+        self.ageLabel.text = "\(myTiger.age)"
+        self.breedLabel.text = myTiger.breed
+        self.randomFactLabel.text = myTiger.randomFact()
+
+        //JB : !!! Variable which is of type Tiger - only locally available within the function
         var secondTiger = Tiger()
         secondTiger.name = "Tigress"
         secondTiger.breed = "Indochinese Tiger"
         secondTiger.age = 2
         secondTiger.image = UIImage(named:"IndochineseTiger.jpg")
+        secondTiger.age = secondTiger.ageInTigerYearsFromAge(secondTiger.age)
 
+        //JB : Call two functions declared in class Tiger.swift
+        secondTiger.chuff()
+
+        //JB : !!! Variable which is of type Tiger - only locally available within the function
         var thirdTiger = Tiger()
         thirdTiger.name = "Jacob"
         thirdTiger.breed = "Malayan Tiger"
         thirdTiger.age = 4
         thirdTiger.image = UIImage(named:"MalayanTiger.jpg")
+        thirdTiger.chuff()
 
+        thirdTiger.age = thirdTiger.ageInTigerYearsFromAge(thirdTiger.age)
+
+        //JB : !!! Variable which is of type Tiger - only locally available within the function
         var fourthTiger = Tiger()
         fourthTiger.name = "Spar"
         fourthTiger.breed = "Siberian Tiger"
         fourthTiger.age = 5
         fourthTiger.image = UIImage(named:"SiberianTiger.jpg")
+        fourthTiger.chuff()
 
-        myTigers += [secondTiger, thirdTiger, fourthTiger]
+        fourthTiger.age = fourthTiger.ageInTigerYearsFromAge(fourthTiger.age)
+
+        self.myTigers += [secondTiger, thirdTiger, fourthTiger]
+
+        //JB : Prints out all names of Tigers from the array
+        for test in self.myTigers {
+            println(test.name)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -72,11 +101,12 @@ class ViewController: UIViewController {
         //JB : Ensure that the same tiger is not shown after tab on next...
         var randomIndex:Int
         do {
-            randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
-        } while currentIndex == randomIndex
-        currentIndex = randomIndex
-        let tiger = myTigers[randomIndex]
+            //JB : Creates a random number between 0 and the array length
+            randomIndex = Int(arc4random_uniform(UInt32(self.myTigers.count)))
+        } while self.currentIndex == randomIndex
 
+        self.currentIndex = randomIndex
+        let tiger = self.myTigers[randomIndex]
 
 //        myImageView.image = tiger.image
 //        nameLabel.text = tiger.name
@@ -86,10 +116,12 @@ class ViewController: UIViewController {
         //JB : Build a nice fade animation after the tab on the next button
         UIView.transitionWithView(self.view, duration: 2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
 
+                //JB : tiger is declared above - let tiger = self.myTigers[randomIndex]
                 self.myImageView.image = tiger.image
                 self.nameLabel.text = tiger.name
                 self.ageLabel.text = "\(tiger.age)"
                 self.breedLabel.text = tiger.breed
+                self.randomFactLabel.text = tiger.randomFact()
 
             }, completion: {
                 (finished: Bool) -> () in
